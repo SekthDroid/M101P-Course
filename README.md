2c74076aebab912e6f49fe271733a770fbe285cf
SekthDroid <secthndamon@gmail.com>
2015-10-16 00:41:42 +0200

    Added readme and Hello world
    Hello world for the Python framework called “Bottle”

---
+++ README.md
@@ -0,0 +1,4 @@
+
+<h1>M101P: MONGODB for developers</b>
+
+<p>All resources I am using for the M101P course by <a href="https://university.mongodb.com">MongoDB University</a>
---
+++ week1/hello_bottle.py
@@ -0,0 +1,10 @@
+__author__ = 'SekthDroid'
+
+from bottle import route, run, template
+
+
+@route('/hello/<name>')
+def hello(name):
+    return template("<b>Hello {{name}}</b>!", name=name)
+
+run(host='localhost', port=8080, debug=True)
