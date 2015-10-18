16eed1bd9be994c4671a75ab5e1875c2f8d16433
SekthDroid <secthndamon@gmail.com>
2015-10-18 13:21:18 +0200

    Changed templates to the new templates folder within python code

--- week1/hello_bottle_things.py
+++ week1/hello_bottle_things.py
@@ -5,7 +5,7 @@ import bottle
 @bottle.route('/')
 def home():
     mythings = ["apple", "orange", "banana", "peach"]
-    return bottle.template('hello_world', username="SekthDroid", things=mythings)
+    return bottle.template('templates/hello_world', username="SekthDroid", things=mythings)
 
 bottle.debug(True)
 bottle.run(host='localhost', port=8080)
